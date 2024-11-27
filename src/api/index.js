5b1a56e1307e8d580f0e339b2e875c64a5be0508
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-11-27 13:48:19 +0600

    Service section almost done only need style

--- src/api/index.js
+++ src/api/index.js
@@ -44,3 +44,25 @@ export const companyLogos = [
         link: "#",
     }
 ];
+
+
+
+
+// service info of flexibility
+export const serviceInfo = [
+    {
+        serviceImg: "/img/icon (1).png",
+        title: "You’re in Control",
+        description: "aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work."
+    },
+    {
+        serviceImg: "/img/icon (2).png",
+        title: "Infinitely Scalable",
+        description: "Train an aiDR on a patch. When you are happy with the results, scale it effortlessly."
+    },
+    {
+        serviceImg: "/img/icon (3).png",
+        title: "Incredibly Flexible",
+        description: "Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time."
+    },
+];
--- src/pages/Home/Service.jsx
+++ src/pages/Home/Service.jsx
@@ -1,9 +1,25 @@
+import { serviceInfo } from "../../api";
+import Container from "../../components/shared/Container/Container";
 
 const Service = () => {
     return (
-        <div>
-            Service
-        </div>
+        <section>
+            <Container>
+                <div className="flex flex-col md:flex-row justify-between items-center gap-6">
+                    {
+                        serviceInfo?.map(({serviceImg, title, description}, idx) => (
+                            <div key={idx} className="md:w-[33%] lg:w-[384px]">
+                                <img src={serviceImg} alt="" />
+                                <div>
+                                    <h2>{title}</h2>
+                                    <p>{description}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Container>
+        </section>
     );
 };
 
