57eedf2201bdd1162313ff39ff15851b154c4260
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-11-27 14:18:27 +0600

    Service section now fully perfect and responsive for small device lg, xl

--- src/pages/Home/Service.jsx
+++ src/pages/Home/Service.jsx
@@ -5,15 +5,15 @@ const Service = () => {
     return (
         <section>
             <Container>
-                <div className="flex flex-col md:flex-row justify-between items-center gap-6">
+                <div className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-6 py-[84px]">
                     {
                         serviceInfo?.map(({serviceImg, title, description}, idx) => (
-                            <div key={idx} className="md:w-[33%] lg:w-[384px]">
-                                <img src={serviceImg} alt="" />
+                            <div key={idx} className="md:w-[33%] lg:w-[360px] xl:w-[384px] flex flex-col justify-between gap-4">
                                 <div>
-                                    <h2>{title}</h2>
-                                    <p>{description}</p>
+                                    <img src={serviceImg} alt="" />
                                 </div>
+                                <h2 className="text-xl md:text-2xl lg:text-[28px] font-semibold font-montserrat">{title}</h2>
+                                <p className="font-worksans text-base lg:text-xl xl:text-[22px] font-light">{description}</p>
                             </div>
                         ))
                     }
